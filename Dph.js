819de9da19998e5e2e6d655175e2e98bf684439b
mihawk6 <89507063+mihawk6@users.noreply.github.com>
2022-01-24 23:02:20 +0530

    .

--- Dph.js
+++ Dph.js
@@ -106,3 +106,12 @@
             console.log("Record added: " + x.elements[0].value)
             empDetails()
         }
+
+        function validateForm()
+         {
+            let x = document.forms["myForm"]["NAME"].value;
+            if (x == "") {
+              alert("Name must be filled out");
+              return false;
+            }
+          }
